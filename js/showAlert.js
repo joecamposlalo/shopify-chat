62ddb42d649a328586c24471b5340363072871ca
joelcamposlalo <joelcamposlalo@gmail.com>
2022-06-14 11:19:58 -0500

    Landing

---
+++ js/showAlert.js
@@ -0,0 +1,3 @@
+document.getElementById('showTerms').onclick = function(){
+    Swal.fire('The 20 hours of FREE design & web development will be added as credit to the first month of services when the client signs 3-month contract.')
+}
