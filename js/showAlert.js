b0ad004bed08e3cb96fc1480ddfe276621f847a2
Land <76625399+Landbios@users.noreply.github.com>
2022-06-20 09:54:42 -0400

    actualizacion landing page terminada

--- js/showAlert.js
+++ js/showAlert.js
@@ -1,3 +1,7 @@
 document.getElementById('showTerms').onclick = function(){
-    Swal.fire('The 20 hours of FREE design & web development will be added as credit to the first month of services when the client signs 3-month contract.')
+    Swal.fire({
+        title: 'The 20 hours of FREE design & web development will be added as credit to the first month of services.',
+        confirmButtonText: 'Close'
+
+    })
 }
