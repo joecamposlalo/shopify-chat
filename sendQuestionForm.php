f3b8a225dcb67aff9bd3e2488c2275d403c21284
Land <76625399+Landbios@users.noreply.github.com>
2022-06-22 10:48:48 -0400

    actualizacion repositorio

---
+++ sendQuestionForm.php
@@ -0,0 +1,14 @@
+<?php
+
+$to      = 'do@solvedpuzzle.com';
+$subject = 'Question from Shopify Chat';
+$message = trim(filter_var($_POST['question'],FILTER_SANITIZE_STRING));
+$name = trim(filter_var($_POST['first_name']." ".$_POST['last_name'].", ".$_POST['business'],FILTER_SANITIZE_STRING));
+$from = filter_var($_POST['email'],FILTER_SANITIZE_EMAIL);
+
+$headers = "From:$name<$from>". "\r\n";    
+echo mail($to, $subject, $message, $headers);
+	
+
+
+?>
